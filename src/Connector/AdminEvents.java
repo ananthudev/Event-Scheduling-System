57bfadb09d0e3159b261aeac540e29ac9f296155
ananthudev <ananthudevdv@gmail.com>
2023-06-07 02:37:05 +0530

    added admin event, fields get autopopulated, no refresh or approve

--- src/Connector/AdminEvents.java
+++ src/Connector/AdminEvents.java
@@ -19,163 +19,221 @@ import javax.swing.border.BevelBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.table.DefaultTableModel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AdminEvents {
 
-    private JFrame frmAdminEvents;
-    private JTextField textField;
-    private JTextField textField_2;
-    private JTextField textField_3;
-    private JTextField textField_4;
-    private JTextField textField_6;
-    private JTable table;
+	private JFrame frmAdminEvents;
+	private JTextField textField;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_6;
+	private JTable table;
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    AdminEvents window = new AdminEvents();
-                    window.frmAdminEvents.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	private Connection connection;
+	private DefaultTableModel tableModel;
 
-    /**
-     * Create the application.
-     */
-    public AdminEvents() {
-        initialize();
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminEvents window = new AdminEvents();
+					window.frmAdminEvents.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() {
-        frmAdminEvents = new JFrame();
-        frmAdminEvents.getContentPane().setForeground(new Color(255, 255, 255));
-        frmAdminEvents.setTitle("Admin Events");
-        frmAdminEvents.getContentPane().setBackground(new Color(0, 128, 128));
-        frmAdminEvents.getContentPane().setLayout(null);
+	/**
+	 * Create the application.
+	 */
+	public AdminEvents() {
+		initialize();
+		establishConnection();
+		populateTable();
+	}
 
-        JLabel lblEndTime_2 = new JLabel("End Time");
-        lblEndTime_2.setForeground(Color.WHITE);
-        lblEndTime_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        lblEndTime_2.setBounds(73, 406, 165, 60);
-        frmAdminEvents.getContentPane().add(lblEndTime_2);
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmAdminEvents = new JFrame();
+		frmAdminEvents.getContentPane().setForeground(new Color(255, 255, 255));
+		frmAdminEvents.setTitle("Admin Events");
+		frmAdminEvents.getContentPane().setBackground(new Color(0, 128, 128));
+		frmAdminEvents.getContentPane().setLayout(null);
+		
+		JLabel lblEndTime_2 = new JLabel("End Time");
+		lblEndTime_2.setForeground(Color.WHITE);
+		lblEndTime_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEndTime_2.setBounds(30, 380, 165, 60);
+		frmAdminEvents.getContentPane().add(lblEndTime_2);
+		
+		table = new JTable();
+		tableModel = new DefaultTableModel(
+			new Object[][] {},
+			new String[] {
+				"Department", "Event Name", "Hall", "Start Time", "End Time"
+			}
+		);
+		table.setModel(tableModel);
+		table.getColumnModel().getColumn(0).setPreferredWidth(72);
+		table.getColumnModel().getColumn(2).setPreferredWidth(63);
+		table.getColumnModel().getColumn(3).setPreferredWidth(137);
+		table.getColumnModel().getColumn(4).setPreferredWidth(151);
+		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 
-        table = new JTable();
-        DefaultTableModel model = new DefaultTableModel(
-            new Object[][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-            },
-            new String[] {
-                "Department", "Event Name", "Hall", "Start Time", "End Time"
-            }
-        );
-        table.setModel(new DefaultTableModel(
-        	new Object[][] {
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        		{null, null, null, null, null},
-        	},
-        	new String[] {
-        		"Department", "Event Name", "Hall", "Start Time", "End Time"
-        	}
-        ));
-        table.getColumnModel().getColumn(2).setPreferredWidth(73);
-        table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(398, 244, 494, 162);
+		frmAdminEvents.getContentPane().add(scrollPane);
 
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(456, 290, 373, 128);
-        frmAdminEvents.getContentPane().add(scrollPane);
+		JLabel lblAdminEvents = new JLabel("Admin Events");
+		lblAdminEvents.setForeground(Color.WHITE);
+		lblAdminEvents.setFont(new Font("Tahoma", Font.PLAIN, 38));
+		lblAdminEvents.setBounds(299, 95, 283, 43);
+		frmAdminEvents.getContentPane().add(lblAdminEvents);
 
-        JLabel lblAdminEvents = new JLabel("Admin Events");
-        lblAdminEvents.setForeground(Color.WHITE);
-        lblAdminEvents.setFont(new Font("Tahoma", Font.PLAIN, 38));
-        lblAdminEvents.setBounds(299, 95, 283, 43);
-        frmAdminEvents.getContentPane().add(lblAdminEvents);
+		JLabel lblStartTime = new JLabel("Start Time");
+		lblStartTime.setForeground(Color.WHITE);
+		lblStartTime.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblStartTime.setBounds(30, 346, 165, 60);
+		frmAdminEvents.getContentPane().add(lblStartTime);
+		
+		table.getSelectionModel().addListSelectionListener(e -> {
+			int selectedRow = table.getSelectedRow();
+			if (selectedRow != -1) {
+				String department = table.getValueAt(selectedRow, 0) != null ? table.getValueAt(selectedRow, 0).toString() : "";
+				String eventName = table.getValueAt(selectedRow, 1) != null ? table.getValueAt(selectedRow, 1).toString() : "";
+				String hall = table.getValueAt(selectedRow, 2) != null ? table.getValueAt(selectedRow, 2).toString() : "";
+				String startTime = table.getValueAt(selectedRow, 3) != null ? table.getValueAt(selectedRow, 3).toString() : "";
+				String endTime = table.getValueAt(selectedRow, 4) != null ? table.getValueAt(selectedRow, 4).toString() : "";
 
-        JLabel lblNewLabel = new JLabel("Department");
-        lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        lblNewLabel.setForeground(new Color(255, 255, 255));
-        lblNewLabel.setBounds(73, 264, 116, 60);
-        frmAdminEvents.getContentPane().add(lblNewLabel);
+				textField.setText(department);
+				textField_2.setText(eventName);
+				textField_3.setText(hall);
+				textField_4.setText(startTime);
+				textField_6.setText(endTime);
+			}
+		});
 
-        JLabel lblRequestedHall = new JLabel("Hall Requested");
-        lblRequestedHall.setForeground(Color.WHITE);
-        lblRequestedHall.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        lblRequestedHall.setBounds(73, 335, 165, 60);
-        frmAdminEvents.getContentPane().add(lblRequestedHall);
+		JButton btnAdd = new JButton("Approve");
+		btnAdd.setForeground(new Color(0, 0, 0));
+		btnAdd.setBackground(new Color(255, 255, 255));
+		btnAdd.setBounds(74, 464, 100, 32);
+		frmAdminEvents.getContentPane().add(btnAdd);
+		
+		JButton btnUpdate = new JButton("Deny");
+		btnUpdate.setForeground(Color.BLACK);
+		btnUpdate.setBackground(Color.WHITE);
+		btnUpdate.setBounds(214, 464, 100, 32);
+		frmAdminEvents.getContentPane().add(btnUpdate);
+		
+		JButton btnDelete = new JButton("Refresh");
+		btnDelete.setForeground(Color.BLACK);
+		btnDelete.setBackground(Color.WHITE);
+		btnDelete.setBounds(589, 428, 100, 32);
+		frmAdminEvents.getContentPane().add(btnDelete);
+						
+		textField_3 = new JTextField();
+		textField_3.setBounds(158, 325, 205, 23);
+		frmAdminEvents.getContentPane().add(textField_3);
+		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_3.setColumns(10);
+								
+		textField_4 = new JTextField();
+		textField_4.setBounds(158, 362, 205, 23);
+		frmAdminEvents.getContentPane().add(textField_4);
+		textField_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_4.setColumns(10);
+										
+		textField_6 = new JTextField();
+		textField_6.setBounds(158, 396, 205, 23);
+		frmAdminEvents.getContentPane().add(textField_6);
+		textField_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_6.setColumns(10);
+												
+		textField = new JTextField();
+		textField.setBounds(158, 241, 205, 23);
+		frmAdminEvents.getContentPane().add(textField);
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField.setColumns(10);
+														
+		JLabel lblNewLabel = new JLabel("Department");
+		lblNewLabel.setBounds(30, 225, 116, 60);
+		frmAdminEvents.getContentPane().add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+																
+		JLabel lblEventName = new JLabel("Event Name");
+		lblEventName.setBounds(30, 267, 116, 60);
+		frmAdminEvents.getContentPane().add(lblEventName);
+		lblEventName.setForeground(Color.WHITE);
+		lblEventName.setFont(new Font("Tahoma", Font.PLAIN, 18));
+																		
+		textField_2 = new JTextField();
+		textField_2.setBounds(158, 287, 205, 23);
+		frmAdminEvents.getContentPane().add(textField_2);
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_2.setColumns(10);
+																				
+		JLabel lblRequestedHall = new JLabel("Hall Requested");
+		lblRequestedHall.setBounds(30, 309, 144, 60);
+		frmAdminEvents.getContentPane().add(lblRequestedHall);
+		lblRequestedHall.setForeground(Color.WHITE);
+		lblRequestedHall.setFont(new Font("Tahoma", Font.PLAIN, 18));
 
-        JLabel lblEndTime = new JLabel("Start Time");
-        lblEndTime.setForeground(Color.WHITE);
-        lblEndTime.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        lblEndTime.setBounds(73, 371, 165, 60);
-        frmAdminEvents.getContentPane().add(lblEndTime);
+		frmAdminEvents.setBounds(100, 100, 914, 720);
+		frmAdminEvents.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
-        textField = new JTextField();
-        textField.setColumns(10);
-        textField.setBounds(229, 287, 132, 20);
-        frmAdminEvents.getContentPane().add(textField);
+	/**
+	 * Establishes the database connection.
+	 */
+	private void establishConnection() {
+		String url = "jdbc:mysql://localhost:3306/ess";
+		String username = "root";
+		String password = "system";
 
-        textField_2 = new JTextField();
-        textField_2.setColumns(10);
-        textField_2.setBounds(229, 358, 132, 20);
-        frmAdminEvents.getContentPane().add(textField_2);
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-        textField_3 = new JTextField();
-        textField_3.setColumns(10);
-        textField_3.setBounds(229, 394, 132, 20);
-        frmAdminEvents.getContentPane().add(textField_3);
+	/**
+	 * Populates the table with data from the database.
+	 */
+	private void populateTable() {
+	    try {
+	        String query = "SELECT * FROM events";
+	        PreparedStatement statement = connection.prepareStatement(query);
+	        ResultSet resultSet = statement.executeQuery();
 
-        textField_4 = new JTextField();
-        textField_4.setColumns(10);
-        textField_4.setBounds(229, 429, 132, 20);
-        frmAdminEvents.getContentPane().add(textField_4);
+	        while (resultSet.next()) {
+	            String department = resultSet.getString("department");
+	            String eventName = resultSet.getString("eventname");
+	            String hall = resultSet.getString("hall");
+	            String eventStart = resultSet.getString("eventstart");
+	            String eventEnd = resultSet.getString("eventend");
+	            Object[] row = { department, eventName, hall, eventStart, eventEnd };
+	            tableModel.addRow(row);
+	        }
 
-        JButton btnNewButton = new JButton("Approve");
-        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        btnNewButton.setBounds(83, 477, 89, 23);
-        frmAdminEvents.getContentPane().add(btnNewButton);
-
-        JButton btnDeny = new JButton("Deny");
-        btnDeny.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        btnDeny.setBounds(204, 477, 89, 23);
-        frmAdminEvents.getContentPane().add(btnDeny);
-
-        JLabel lblEventName = new JLabel("Event Name");
-        lblEventName.setForeground(Color.WHITE);
-        lblEventName.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        lblEventName.setBounds(73, 300, 116, 60);
-        frmAdminEvents.getContentPane().add(lblEventName);
-
-        textField_6 = new JTextField();
-        textField_6.setColumns(10);
-        textField_6.setBounds(229, 327, 132, 20);
-        frmAdminEvents.getContentPane().add(textField_6);
-
-        JButton btnReset = new JButton("Reset");
-        btnReset.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        btnReset.setBounds(319, 479, 89, 23);
-        frmAdminEvents.getContentPane().add(btnReset);
-        frmAdminEvents.setBounds(100, 100, 919, 703);
-        frmAdminEvents.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	        resultSet.close();
+	        statement.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
 }

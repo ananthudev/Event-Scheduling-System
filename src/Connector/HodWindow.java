5dba81808670fc42183377535febe3226ba23ab1
ananthudev <ananthudevdv@gmail.com>
2023-06-09 03:48:32 +0530

    Hod window shows which department HOD is from and Username

--- src/Connector/HodWindow.java
+++ src/Connector/HodWindow.java
@@ -1,28 +1,32 @@
 package Connector;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import java.awt.Color;
-import javax.swing.JLabel;
+import java.awt.EventQueue;
 import java.awt.Font;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class HodWindow {
 
     private JFrame frame;
+    private String username;
+    private static String department;
 
-    /**
-     * Launch the application.
-     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    HodWindow window = new HodWindow();
-                    window.frame.setVisible(true);
+                    HodWindow window = new HodWindow("username", department);
+                    window.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -30,21 +34,42 @@ public class HodWindow {
         });
     }
 
-    /**
-     * Create the application.
-     */
-    public HodWindow() {
+    public HodWindow(String username, String department2) {
+        this.username = username;
+        HodWindow.department = fetchDepartment(username);
         initialize();
     }
 
-    /**
-     * Initialize the contents of the frame.
-     */
+    private String fetchDepartment(String username) {
+        String department = "";
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ess", "root", "system");
+            PreparedStatement statement = conn.prepareStatement("SELECT department FROM reg WHERE username = ?");
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                department = resultSet.getString("department");
+            }
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return department;
+    }
+
     private void initialize() {
         frame = new JFrame();
         frame.setTitle("HOD Panel");
         frame.getContentPane().setBackground(new Color(0, 128, 128));
         frame.getContentPane().setLayout(null);
+
+        JLabel lblDepartment = new JLabel("Department: " + department);
+        lblDepartment.setForeground(Color.WHITE);
+        lblDepartment.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblDepartment.setBounds(50, 10, 500, 25);
+        frame.getContentPane().add(lblDepartment);
 
         JLabel lblHodPanel = new JLabel("HOD Panel");
         lblHodPanel.setForeground(Color.WHITE);
@@ -52,15 +77,19 @@ public class HodWindow {
         lblHodPanel.setBounds(301, 102, 214, 43);
         frame.getContentPane().add(lblHodPanel);
 
+        JLabel lblUserInfo = new JLabel("Username: " + username);
+        lblUserInfo.setForeground(Color.WHITE);
+        lblUserInfo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblUserInfo.setBounds(50, 50, 500, 25);
+        frame.getContentPane().add(lblUserInfo);
+
         JButton btnCreateRequest = new JButton("Create Request");
         btnCreateRequest.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		
-//        		// Open the HodRequest window
-//                HodRequest hodRequest = new HodRequest();
-//                hodRequest.frame.setVisible(true);
-//                frame.dispose(); // Close the current window
-        	}
+            public void actionPerformed(ActionEvent e) {
+                HodRequest hodRequest = new HodRequest(username, department);
+                hodRequest.setVisible(true);
+                frame.dispose();
+            }
         });
         btnCreateRequest.setFont(new Font("Tahoma", Font.PLAIN, 20));
         btnCreateRequest.setBounds(118, 312, 223, 86);
@@ -71,24 +100,24 @@ public class HodWindow {
         btnViewAllEvents.setBounds(456, 312, 223, 86);
         frame.getContentPane().add(btnViewAllEvents);
 
-        JButton btnNewButton_1 = new JButton("Log Out");
-        btnNewButton_1.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		
-        	login login = new login();
+        JButton btnLogOut = new JButton("Log Out");
+        btnLogOut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Login login = new Login();
                 login.setVisible(true);
-                frame.dispose(); 
-        	}
+                frame.dispose();
+            }
         });
-        btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        btnNewButton_1.setBounds(638, 516, 135, 58);
-        frame.getContentPane().add(btnNewButton_1);
+        btnLogOut.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btnLogOut.setBounds(638, 516, 135, 58);
+        frame.getContentPane().add(btnLogOut);
+
         frame.setBounds(100, 100, 874, 703);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public void setVisible(boolean b) {
         frame.setVisible(b);
     }
-
 }
